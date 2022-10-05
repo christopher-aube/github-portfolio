3f84b14dfaad97cb942189b826a9f13ec76b88d6
aubec-q4 <chris@chrisaube.com>
2022-10-05 10:18:38 -0400

    UPDATE - added app foundation

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const App = () => {
+
+  return (
+    <main className="container">
+      <h1 className="heading">Hello World</h1>
+    </main>
+  );
+};
+
+export default {
+  App,
+};
